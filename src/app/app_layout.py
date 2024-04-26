e8e305159d744429c98d457123f1bca99f4022e0
Fabio Rodrigues <fabio.rod@outlook.pt>
2024-04-26 15:59:04 +0100

    Transição entre paginas completa, Fix Commit anterior

--- src/app/app_layout.py
+++ src/app/app_layout.py
@@ -1,38 +1,52 @@
 import flet as ft
-from flet import Text, Column, UserControl, icons, Control, IconButton, colors
+from flet import Text, Column, icons, Control, IconButton, colors
 
 from app.app_pages import AppPages
 from app.features.app_menu import AppMenu
-from models.user.auth.UserAuthentication import UserAuthentication
+from app.features.app_product_description import ProductDescription
 from shared.Base.SharedControls import SharedControls
 
 
 class AppLayout(SharedControls):
-    """
+    """ """
 
-    """
-    def __init__(self, *args, **kwargs):
+    product_details: ft.BottomSheet
+    app_product_description = ProductDescription()
+
+    def __init__(self, page, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.page = page
+        self.details()
         self.app_menu = AppMenu(self.page)
         self.app_pages = AppPages(self.page, self.user)
 
         pages = [
             (
-                self.app_menu.create_menu_btn(label_='stock', icon_=icons.WAREHOUSE_OUTLINED),
-                self.app_pages.create_content(view=0),
+                self.app_menu.create_menu_btn(
+                    label_="Stock", icon_=icons.WAREHOUSE_OUTLINED
+                ),
+                self.app_pages.create_content(
+                    view=0, row=self.row_selected, table="Stock"
+                ),
             ),
             (
-                self.app_menu.create_menu_btn(label_='supplier', icon_=icons.PERSON_2),
-                self.app_pages.create_content(view=1),
+                self.app_menu.create_menu_btn(label_="Supplier", icon_=icons.PERSON_2),
+                self.app_pages.create_content(
+                    view=1, row=self.row_selected, table="Supplier"
+                ),
             ),
             (
-                self.app_menu.create_menu_btn(label_='User', icon_=icons.PERSON),
-                self.app_pages.create_content(view=2),
+                self.app_menu.create_menu_btn(label_="User", icon_=icons.PERSON),
+                self.app_pages.create_content(
+                    view=2, row=self.row_selected, table="User"
+                ),
             ),
         ]
 
         self.navigation_items = [navigation_item for navigation_item, _ in pages]
-        self.navigation_rail = self.app_menu.build_navigation_rail(self._navigation_change)
+        self.navigation_rail = self.app_menu.build_navigation_rail(
+            self._navigation_change
+        )
         self.update_destinations()
         self._menu_extended = True
         self.navigation_rail.extended = True
@@ -44,41 +58,115 @@ class AppLayout(SharedControls):
         self.content_area = Column(controls=page_contents, expand=True)
 
         self.toggle_menu_panel = IconButton(
-            icon=icons.ARROW_CIRCLE_LEFT, icon_color=colors.BLUE_GREY_400, selected=False,
-            selected_icon=icons.ARROW_CIRCLE_RIGHT, on_click=self.toggle_menu_panel)
+            icon=icons.ARROW_CIRCLE_LEFT,
+            icon_color=colors.BLUE_GREY_400,
+            selected=False,
+            selected_icon=icons.ARROW_CIRCLE_RIGHT,
+            on_click=self.toggle_menu_panel,
+        )
 
         self.toggle_search_panel = IconButton(
-            icon=icons.ARROW_CIRCLE_RIGHT, icon_color=colors.BLUE_GREY_400, selected=True,
-            selected_icon=icons.ARROW_CIRCLE_LEFT, on_click=self.toggle_search_panel)
+            icon=icons.ARROW_CIRCLE_RIGHT,
+            icon_color=colors.BLUE_GREY_400,
+            selected=True,
+            selected_icon=icons.ARROW_CIRCLE_LEFT,
+            on_click=self.toggle_search_panel,
+        )
 
-        self._active_view: Control = Column(controls=[
-            Text("")
-        ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+        self._active_view: Control = Column(
+            controls=[Text("")],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
 
         self.set_content()
         self._change_displayed_page()
 
-    def select_page(self, page_number):
-        self.navigation_rail.selected_index = page_number
-        self._change_displayed_page()
+    @classmethod
+    async def row_selected(cls, event):
+        col_names = {
+            "Stock": ["product_name", "product_bar_cod", "supplier_name", "quantity"],
+            "Supplier": ["name", "address", "phone", "email"],
+            "User": ["name", "login", "password", "typeAccess"],
+        }
+        selected = {}
+        for cells, col_name in zip(event.control.cells, col_names[event.control.data]):
+            selected[col_name] = cells.content.value
+        await cls.show_bs(selected, event.control.data)
+
+    @classmethod
+    def details(cls):
+        cls.product_details = ft.BottomSheet(
+            open=False,
+            on_dismiss=cls.bs_dismissed,
+            elevation=0.7,
+            maintain_bottom_view_insets_padding=True,
+            use_safe_area=True,
+        )
+
+    @classmethod
+    def bs_dismissed(cls, e):
+        print("Dismissed!")
+
+    @classmethod
+    async def close_bs(cls, e):
+        cls.product_details.open = False
+        await cls.product_details.update_async()
+
+    @classmethod
+    async def show_bs(cls, selected, table):
+        # Alterar o description do Supplier e do User
+        details = {
+            "Stock": cls.app_product_description.get_content(selected),
+            "Supplier": cls.app_product_description.get_content(selected),
+            "User": cls.app_product_description.get_content(selected),
+        }
+        content = details[table]
+        cls.product_details.content = content
+        cls.product_details.open = True
+        await cls.product_details.update_async()
+
+    # happens when example is added to the page (when user chooses the BottomSheet control from the grid)
+    def did_mount(self):
+        self.page.overlay.append(self.product_details)
+        self.page.update()
+
+    # happens when example is removed from the page (when user chooses different control group on the navigation rail)
+    def will_unmount(self):
+        self.page.overlay.remove(self.product_details)
+        self.page.update()
+
+    # def select_page(self, page_number):
+    #     self.navigation_rail.selected_index = page_number
+    #     self._change_displayed_page()
 
     def _navigation_change(self, e):
         self._change_displayed_page()
-        self.page.update()
+        # self.content_area.update()
+        # self.page.update()
 
     def _change_displayed_page(self):
         page_number = self.navigation_rail.selected_index
         for i, content_page in enumerate(self.content_area.controls):
             # update selected page
             content_page.visible = page_number == i
+            # self.content_area.update()
+            self.page.update()
 
     def update_destinations(self):
         self.navigation_rail.destinations = self.navigation_items
-        self.navigation_rail.label_type = ft.NavigationRailLabelType.ALL,
+        self.navigation_rail.label_type = (ft.NavigationRailLabelType.ALL,)
 
     def set_content(self, panel_=None):
-        self.controls = [self.menu_panel, self.toggle_menu_panel, self.active_view,
-                         self.content_area, self.toggle_search_panel, self.search_panel]
+        self.controls = [
+            self.menu_panel,
+            self.toggle_menu_panel,
+            self.active_view,
+            self.content_area,
+            self.toggle_search_panel,
+            self.search_panel,
+            self.product_details,
+        ]
         self.update_destinations()
         self.navigation_rail.extended = self._menu_extended
         if panel_:
--- src/main.py
+++ src/main.py
@@ -1,20 +1,36 @@
 import flet
 import flet as ft
-from flet import Container, padding, Page, margin
-from App.app_window import AppWindow
+from flet import Container, padding, margin
+
+from app.app_window import AppWindow
+from shared.Base.SharedControls import SharedControls
 
 
-class App(ft.UserControl):
-    def __init__(self, page: Page):
-        super().__init__()
+class App(SharedControls):
+    def __init__(self, page: ft.Page, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.page = page
+        # dto = ProductDto(name='Alho francês', brand_id=19, category_id=1, price=1.1, photo='alho_frances.png',
+        #                  description='Alho francês')
+        # user = UserAuthentication()
+        # controller = ProductController()
+        # test = controller.update(dto, user)
+        # stoc_dto = StockDto(
+        #     product_name="Alho francês",
+        #     quantity=1300,
+        #     product_bar_cod="019-001-17173184",
+        #     supplier_name="Fornecedor 5",
+        # )
+        # user = UserAuthentication()
+        # controller_2 = StockController()
+        # test = controller_2.update(stoc_dto, user)
         self.app_view = Container(
             expand=True,
             expand_loose=True,
             padding=padding.all(0),
             margin=margin.all(0),
             alignment=ft.alignment.center,
-            content=AppWindow(self, page)
+            content=AppWindow(self.page),
         )
         self.page.add(self.app_view)
         self.page.update()
@@ -34,5 +50,5 @@ def main(page: ft.Page):
     page.add(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ft.app(target=main, view=flet.FLET_APP_WEB, assets_dir="./assets")
